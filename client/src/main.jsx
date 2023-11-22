0426aa3a7577c95918ea6cdd6fca65250695a94d
Clkwong3 <134983608+Clkwong3@users.noreply.github.com>
2023-11-22 06:57:14 -0800

    Client/Src/Main - Add comments so I can understand the logic better

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,45 +1,57 @@
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+// Import necessary dependencies from React and ReactDOM
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-import App from './App.jsx';
-import Home from './pages/Home';
-import Detail from './pages/Detail';
-import NoMatch from './pages/NoMatch';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import Success from './pages/Success';
-import OrderHistory from './pages/OrderHistory';
+// Import routing components from react-router-dom
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+// Import components for different pages
+import App from "./App.jsx";
+import Home from "./pages/Home";
+import Detail from "./pages/Detail";
+import NoMatch from "./pages/NoMatch";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Success from "./pages/Success";
+import OrderHistory from "./pages/OrderHistory";
+
+// Create a BrowserRouter instance with route configurations and define the routes
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <App />,
     error: <NoMatch />,
     children: [
       {
-        index: true, 
-        element: <Home />
-      }, {
-        path: '/login',
-        element: <Login />
-      }, {
-        path: '/signup',
-        element: <Signup />
-      }, {
-        path: '/success',
-        element: <Success />
-      }, {
-        path: '/orderHistory',
-        element: <OrderHistory />
-      }, {
-        path: '/products/:id',
-        element: <Detail />
-      }
-    ]
-  }
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/signup",
+        element: <Signup />,
+      },
+      {
+        path: "/success",
+        element: <Success />,
+      },
+      {
+        path: "/orderHistory",
+        element: <OrderHistory />,
+      },
+      {
+        path: "/products/:id",
+        element: <Detail />,
+      },
+    ],
+  },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// Use ReactDOM.createRoot to render the app inside the root element
+ReactDOM.createRoot(document.getElementById("root")).render(
+  // Provide the router to the app using RouterProvider
   <RouterProvider router={router} />
-)
+);
