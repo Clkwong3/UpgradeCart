86b89d8573792bf55dcdc411354e3b9de2de33a4
Clkwong3 <134983608+Clkwong3@users.noreply.github.com>
2023-11-22 05:09:12 -0800

    Server/Model/Category - add comments to explain the code for me to reference later

--- server/models/Category.js
+++ server/models/Category.js
@@ -1,8 +1,9 @@
-const mongoose = require('mongoose');
+// Define a user schema using Mongoose Schema
+const { Schema } = require("mongoose");
 
-const { Schema } = mongoose;
-
+// Define the category schema
 const categorySchema = new Schema({
+  // Name of the category
   name: {
     type: String,
     required: true,
@@ -10,6 +11,8 @@ const categorySchema = new Schema({
   }
 });
 
+// Create a Mongoose model named 'Category' using the categorySchema
 const Category = mongoose.model('Category', categorySchema);
 
+// Export the Category model for use in other files
 module.exports = Category;
