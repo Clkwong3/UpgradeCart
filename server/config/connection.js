f2b617d7dacffb1a8159d4c37a3c1d0ccb7f5120
Clkwong3 <134983608+Clkwong3@users.noreply.github.com>
2023-11-22 05:16:49 -0800

    Server/Config/Connection - add comments to explain the code for me to reference later

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,10 @@
+// Import the mongoose library
 const mongoose = require('mongoose');
 
+// Connect to the MongoDB database using the provided URI (Uniform Resource Identifier)
+// If the process.env.MONGODB_URI variable is defined (e.g., in a production environment),
+// use it as the URI; otherwise, use a default local URI for development purposes.
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mern-shopping');
 
+// Export the mongoose connection object
 module.exports = mongoose.connection;
