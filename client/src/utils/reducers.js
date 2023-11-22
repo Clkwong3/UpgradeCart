cdbd395ec293ef4af9a8ec1c338434b6c44d4c32
Clkwong3 <134983608+Clkwong3@users.noreply.github.com>
2023-11-22 06:34:55 -0800

    Client/Src/Utils/Reducers - Made sure to use the spread operator (...) to avoid mutating the state and add comments about the state properties

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -1,3 +1,4 @@
+// Import action types/constants from actions.js
 import {
   UPDATE_PRODUCTS,
   ADD_TO_CART,
@@ -8,86 +9,116 @@ import {
   UPDATE_CURRENT_CATEGORY,
   CLEAR_CART,
   TOGGLE_CART,
-} from './actions';
+} from "./actions";
 
-// TODO: To get a better understand of how a reducer works - add comments to the various actions in the reducer
-export const reducer = (state, action) => {
+// Define the initial state of your application
+const initialState = {
+  products: [],
+  cart: [],
+  cartOpen: false,
+  categories: [],
+  currentCategory: "",
+};
+
+// Define the reducer function that handles state updates
+export const reducer = (state = initialState, action) => {
+  // Switch statement to handle different action types
   switch (action.type) {
-    // TODO: Add a comment describing the functionality of the UPDATE_PRODUCTS case
-    // Your comment here
     case UPDATE_PRODUCTS:
+      // Action type: UPDATE_PRODUCTS
+      // Update the products array with new data
       return {
-        ...state,
-        products: [...action.products],
+        ...state, // Spread the current state to avoid mutation
+        products: [...action.products], // Replace the products array with new data
       };
 
     case ADD_TO_CART:
+      // Action type: ADD_TO_CART
+      // Add a product to the cart
       return {
-        ...state,
-        cartOpen: true,
-        cart: [...state.cart, action.product],
+        ...state, // Create a new state object with the existing state properties
+        cartOpen: true, // Set cartOpen to true to indicate that the cart is now open
+        cart: [...state.cart, action.product], // Add the new product to the cart
       };
 
     case ADD_MULTIPLE_TO_CART:
+      // Action type: ADD_MULTIPLE_TO_CART
+      // Add multiple products to the cart
       return {
         ...state,
-        cart: [...state.cart, ...action.products],
+        cart: [...state.cart, ...action.products], // Add multiple products to the cart
       };
-    // TODO: Add a comment describing the functionality of the UPDATE_CART_QUANTITY case
-    // Your comment here
+
     case UPDATE_CART_QUANTITY:
+      // Action type: UPDATE_CART_QUANTITY
+      // Update the quantity of a product in the cart
       return {
         ...state,
         cartOpen: true,
+        // Use map to create a new array based on the existing cart array
         cart: state.cart.map((product) => {
+          // Check if the ID of the current product matches the ID passed in the action
           if (action._id === product._id) {
+            // If there's a match, update the purchaseQuantity of the matching product
             product.purchaseQuantity = action.purchaseQuantity;
           }
+          // Return the product, whether it's the one being updated or not
           return product;
         }),
       };
 
-    // TODO: Add a comment describing the functionality of the REMOVE_FROM_CART case
-    // Your comment here
     case REMOVE_FROM_CART:
+      // Action type: REMOVE_FROM_CART
+      // Remove a product from the cart based on its ID
+      // Use the filter method to create a new array excluding the product to be removed
       let newState = state.cart.filter((product) => {
+        // Check if the ID of the current product does not match the ID passed in the action
         return product._id !== action._id;
       });
 
+      // Update the state with the new cart array and determine if the cart should remain open
       return {
         ...state,
-        cartOpen: newState.length > 0,
-        cart: newState,
+        // Set cartOpen to true if there are items in the new cart, otherwise set it to false
+        cartOpen: newState.length > 0, // If there are items in the new cart, keep the cart open
+        cart: newState, // Update the cart without the removed product, a new array (newState)
       };
 
     case CLEAR_CART:
+      // Action type: CLEAR_CART
+      // Clear the entire cart
       return {
         ...state,
-        cartOpen: false,
-        cart: [],
+        cartOpen: false, // Set cartOpen to false to indicate that the cart is now closed
+        cart: [], // Set the cart to an empty array
       };
 
     case TOGGLE_CART:
+      // Action type: TOGGLE_CART
+      // Toggle the visibility of the cart
       return {
         ...state,
-        cartOpen: !state.cartOpen,
+        cartOpen: !state.cartOpen, // Toggle the value of cartOpen
       };
 
     case UPDATE_CATEGORIES:
+      // Action type: UPDATE_CATEGORIES
+      // Update the categories array with new data
       return {
         ...state,
-        categories: [...action.categories],
+        categories: [...action.categories], // Replace the categories array with new data
       };
 
     case UPDATE_CURRENT_CATEGORY:
+      // Action type: UPDATE_CURRENT_CATEGORY
+      // Update the current category
       return {
         ...state,
-        currentCategory: action.currentCategory,
+        currentCategory: action.currentCategory, // Update the current category
       };
 
-    // TODO: Add a comment describing what the default case is for
-    // Your comment here
     default:
+      // Default case: return the current state if the action type is not recognized
       return state;
   }
 };
