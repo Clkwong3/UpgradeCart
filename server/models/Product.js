7b913a4593f20c15a5b75bb1614162634b2cfb15
Clkwong3 <134983608+Clkwong3@users.noreply.github.com>
2023-11-22 05:05:29 -0800

    Server/Model/Product - add comments to explain the code for me to reference later

--- server/models/Product.js
+++ server/models/Product.js
@@ -1,29 +1,35 @@
-const mongoose = require('mongoose');
+// Define a product schema using Mongoose Schema
+const { Schema } = require("mongoose");
 
-const { Schema } = mongoose;
-
+// Create a new schema for the Product model
 const productSchema = new Schema({
+  // Name of the product
   name: {
     type: String,
     required: true,
     trim: true
   },
+  // Description of the product
   description: {
     type: String
   },
+  // Image URL of the product
   image: {
     type: String
   },
+  // Price of the product with a minimum value
   price: {
     type: Number,
     required: true,
     min: 0.99
   },
+  // Quantity of the product with default value and minimum value
   quantity: {
     type: Number,
     min: 0,
     default: 0
   },
+  // The 'Category' model of the product using its ObjectId
   category: {
     type: Schema.Types.ObjectId,
     ref: 'Category',
@@ -31,6 +37,8 @@ const productSchema = new Schema({
   }
 });
 
+// Create a mongoose model named 'Product' based on the productSchema
 const Product = mongoose.model('Product', productSchema);
 
+// Export the Product model for use in other files
 module.exports = Product;
