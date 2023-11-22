15d519d0f345dccd897f1be12e2ab31a6705717b
Clkwong3 <134983608+Clkwong3@users.noreply.github.com>
2023-11-22 04:58:03 -0800

    Server/Models/User - add comments to explain the code for me to reference later

--- server/models/User.js
+++ server/models/User.js
@@ -1,48 +1,62 @@
-const mongoose = require('mongoose');
+// Define a user schema using Mongoose Schema
+const { Schema } = require("mongoose");
+const bcrypt = require("bcrypt");
 
-const { Schema } = mongoose;
-const bcrypt = require('bcrypt');
-const Order = require('./Order');
+// Import the Order schema
+const Order = require("./Order");
 
+// Define the user schema with various fields
 const userSchema = new Schema({
+  // First name of the user
   firstName: {
     type: String,
     required: true,
-    trim: true
+    trim: true,
   },
+  // Last name of the user
   lastName: {
     type: String,
     required: true,
-    trim: true
+    trim: true,
   },
+  // Email address of the user (unique)
   email: {
     type: String,
     required: true,
-    unique: true
+    unique: true,
   },
+  // Password of the user (hashed and with minimum length)
   password: {
     type: String,
     required: true,
-    minlength: 5
+    minlength: 5,
   },
-  orders: [Order.schema]
+  // Array to store user's orders using the Order schema
+  orders: [Order.schema],
 });
 
-// set up pre-save middleware to create password
-userSchema.pre('save', async function(next) {
-  if (this.isNew || this.isModified('password')) {
+// Middleware: Execute before saving a user to hash the password
+userSchema.pre("save", async function (next) {
+  // Hash the password only if it's new or has been modified
+  if (this.isNew || this.isModified("password")) {
+    // Set the salt rounds for hashing
     const saltRounds = 10;
+    // Hash the password using bcrypt
     this.password = await bcrypt.hash(this.password, saltRounds);
   }
 
+  // Continue with the save operation
   next();
 });
 
-// compare the incoming password with the hashed password
-userSchema.methods.isCorrectPassword = async function(password) {
+// Method to compare an incoming password with the user's hashed password
+userSchema.methods.isCorrectPassword = async function (password) {
+  // Compare the provided password with the stored hashed password
   return await bcrypt.compare(password, this.password);
 };
 
-const User = mongoose.model('User', userSchema);
+// Create a Mongoose model named 'User' using the userSchema
+const User = mongoose.model("User", userSchema);
 
+// Export the User model for use in other files
 module.exports = User;
