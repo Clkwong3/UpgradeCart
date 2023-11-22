526b4ee669fcdac3605d7a19adaf4b33052d3bb4
Clkwong3 <134983608+Clkwong3@users.noreply.github.com>
2023-11-22 02:29:09 -0800

    Server/schema/index.js - Add comments to remind me what they do later on

--- server/schemas/index.js
+++ server/schemas/index.js
@@ -1,4 +1,8 @@
-const typeDefs = require('./typeDefs');
-const resolvers = require('./resolvers');
+// Import the GraphQL type definitions from the 'typeDefs' file
+const typeDefs = require("./typeDefs");
 
+// Import the GraphQL resolvers from the 'resolvers' file
+const resolvers = require("./resolvers");
+
+// Export an object containing the type definitions and resolvers
 module.exports = { typeDefs, resolvers };
